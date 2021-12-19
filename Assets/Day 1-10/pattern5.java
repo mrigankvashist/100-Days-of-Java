1b0bb09de96d94f3e627de36e3c56dbc1f9300fb
Mrigank <mrigankvashist@gmail.com>
2021-12-19 20:44:06 +0530

    Day 5-part 2

--- Assets/Day 1-10/pattern5.java
+++ Assets/Day 1-10/pattern5.java
@@ -4,33 +4,26 @@ public class pattern5 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        int sp = n / 2;
         int st = 1;
-        int sp = n - 3;
-        for (int i = 1; i < n - 2; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= sp; j++) {
                 System.out.print("\t");
             }
             for (int j = 1; j <= st; j++) {
                 System.out.print("*\t");
             }
-
-            System.out.println();
-            st = st + 2;
-            sp = sp - 1;
-        }
-        for (int i = n - 2; i <= n; i++) {
-            for (int j = 1; j <= sp; j++) {
-                System.out.print("\t");
+            if (i <= n / 2) {
+                sp--;
+                st += 2;
+            } else {
+                sp++;
+                st -= 2;
             }
-            for (int j = 1; j <= st; j++) {
-                System.out.print("*\t");
-            }
-
             System.out.println();
-            st = st - 2;
-            sp = sp + 1;
         }
 
         scn.close();
     }
+
 }
