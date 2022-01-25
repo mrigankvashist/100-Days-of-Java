f0aa7a0c886e818113999ed343603f7a85392f90
Mrigank <mrigankvashist@gmail.com>
2022-01-26 03:51:58 +0530

    Day 31 Part 2

---
+++ Assets/DAY 31-40/getstairpath.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class getstairpath {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ArrayList<String> paths = getStairPaths(n);
+        System.out.println(paths);
+        scn.close();
+    }
+
+    public static ArrayList<String> getStairPaths(int n) {
+
+        if (n == 0) {
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        } else if (n < 0) {
+            ArrayList<String> bres = new ArrayList<>();
+            return bres;
+        }
+
+        ArrayList<String> paths1 = getStairPaths(n - 1);
+        ArrayList<String> paths2 = getStairPaths(n - 2);
+        ArrayList<String> paths3 = getStairPaths(n - 3);
+
+        ArrayList<String> paths = new ArrayList<>();
+
+        for (String path : paths1) {
+            paths.add(1 + path);
+        }
+
+        for (String path : paths2) {
+            paths.add(2 + path);
+        }
+
+        for (String path : paths3) {
+            paths.add(3 + path);
+        }
+
+        return paths;
+    }
+
+}
