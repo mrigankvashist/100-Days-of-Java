34c34f99be8cd06e67d0907f99114d874383b7b3
Mrigank <mrigankvashist@gmail.com>
2022-06-06 23:55:59 +0530

    Day 88 Part 2

---
+++ Assets/DAY 81-90/intersectionll.java
@@ -0,0 +1,39 @@
+//commented due to linked list class implementation
+// public class ListNode {
+//          int val;
+//          ListNode next;
+//          ListNode(int x) {
+//              val = x;
+//              next = null;
+//          }
+//      }
+// public class intersectionll{
+// public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//     ListNode p1 = headA, p2 = headB;
+//     int len1 = 0, len2 = 0;
+//     while (p1 != null) {
+//         p1 = p1.next;
+//         len1++;
+//     }
+//     while (p2 != null) {
+//         p2 = p2.next;
+//         len2++;
+//     }
+//     p1 = headA;
+//     p2 = headB;
+//     if (len1 > len2) {
+//         for (int i = 0;i < len1 - len2; i++) {
+//             p1 = p1.next;
+//         }
+//     } else {
+//         for (int i = 0;i < len2 - len1; i++) {
+//             p2 = p2.next;
+//         }
+//     }
+//     while (p1 != p2) {
+//         p1 = p1.next;
+//         p2 = p2.next;
+//     }
+//     return p1;
+// }
+// }
--- README.md
+++ README.md
@@ -1105,4 +1105,7 @@ The n-queens puzzle is the problem of placing n queens on an n x n chessboard su
 Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
 Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
 
+- ### Intersection of Two Linked Lists
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
 
